d20e255444905756c9545571ee89a15e93a8c9a8
Emilia Ziemichód <emifrompoland@gmail.com>
2019-05-10 20:38:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require_relative './course.rb'
 
 class Scraper
 
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -17,8 +17,8 @@ class Scraper
   end
 
 
-    def get_page
+  def get_page
 
-    end
+  end
 
 end
